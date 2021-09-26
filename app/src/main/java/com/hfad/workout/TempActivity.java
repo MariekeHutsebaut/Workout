bac2341b97e76065ff81185fdbbad69f5b2e983b
Marieke Hutsebaut <marieke@tinseltown.be>
2021-09-26 14:38:40 +0200

    finished TempActivity code

--- app/src/main/java/com/hfad/workout/TempActivity.java
+++ app/src/main/java/com/hfad/workout/TempActivity.java
@@ -2,6 +2,7 @@ package com.hfad.workout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.fragment.app.*;
 
 public class TempActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class TempActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temp);
+        if(savedInstanceState == null){
+            StopwatchFragment stopwatch = new StopwatchFragment();
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.add(R.id.stopwatch_container, stopwatch);
+            ft.addToBackStack(null);
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            ft.commit();
+        }
     }
 }
